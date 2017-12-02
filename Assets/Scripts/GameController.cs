4ccd7df9d47a30024f504f53dc4eae5f831853d0
AU <ushakovalexander.it@gmail.com>
2017-12-02 14:56:18 +0200

    Add create new pieces mechanics. Remove and falling animation implementation (using DOTween).

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,69 +4,115 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-    public int columns = 10;
-    public int rows = 10;
-    public float spacing = 1f;
+  public int columns = 10;
+  public int rows = 10;
+  public float spacing = 1f;
 
-    private List<PieceController> _pieces;
+  private List<PieceController> _pieces;
+  private GameObject _piecesParent;
+  private Vector2 _parentOffset;
 
-    void Start () {
-      InitGrid(this.columns, this.rows);
+  void Start() {
+    InitGrid(this.columns, this.rows);
+  }
+
+  private void InitGrid(int columns, int rows) {
+    _piecesParent = new GameObject("PiecesParent");
+    _parentOffset = new Vector2(-columns * 0.5f * spacing + spacing * 0.5f, -rows * 0.5f * spacing + spacing * 0.5f);
+    _piecesParent.transform.position = _parentOffset;
+
+    for(int i = 0; i < columns; i++) {
+      for(int j = 0; j < rows; j++) {
+        InitPiece(i, j);
+      }
     }
+    Debug.Log("Init grid done");
+  }
 
-    private void InitGrid(int columns, int rows) {
-      var pieceParent = new GameObject("PiecesParent");
-      var offset = new Vector2(-columns * 0.5f * spacing + spacing * 0.5f, -rows * 0.5f * spacing  + spacing * 0.5f);
-      pieceParent.transform.position = offset;
+  private PieceController InitPiece(int colIndex, int rowIndex) {
+    if(_piecesParent == null) {
+      Debug.Log("Invalid piece parent");
+      return null;
+    }
+    var position = GetPiecePositionOnGrid(colIndex, rowIndex);
+    var prefab = GetPiecePrefab();
+    PieceController piece = null;
+    if(prefab != null) {
+      piece = Instantiate<PieceController>(prefab, position, Quaternion.identity, _piecesParent.transform);
+      piece.Initialize(colIndex, rowIndex);
+      piece.OnPieceClickedEvent += OnPieceClicked;
+      AddPiece(piece);
+    }
+    return piece;
+  }
 
-      for(int i = 0; i < columns; i++) {
-        for(int j = 0; j < rows; j++) {
-          InitPiece(i, j, pieceParent, offset);
+  private Vector2 GetPiecePositionOnGrid(float colIndex, float rowIndex) {
+    return new Vector2(colIndex * spacing, rowIndex * spacing) + _parentOffset;
+  }
+
+  private void AddPiece(PieceController piece) {
+    if(piece != null) {
+      if(_pieces == null) {
+        _pieces = new List<PieceController>(columns * rows);
+      }
+      if(!_pieces.Contains(piece)) {
+        _pieces.Add(piece);
+      }
+    }
+  }
+
+  private PieceController GetPiece(int colIndex, int rowIndex) {
+    if(_pieces != null) {
+      foreach(PieceController piece in _pieces) {
+        if(piece != null && piece.colIndex == colIndex && piece.rowIndex == rowIndex) {
+          return piece;
         }
       }
-      Debug.Log("Init grid done");
     }
+    return null;
+  }
 
-    private void InitPiece(int colIndex, int rowIndex, GameObject parent, Vector2 offset) {
-      var position = new Vector2(colIndex * spacing, rowIndex * spacing) + offset;
-      var prefab = GetPiecePrefab();
-      if(prefab != null) {
-        var piece = Instantiate<PieceController>(prefab, position, Quaternion.identity, parent.transform);
-        piece.Initialize(colIndex, rowIndex);
-        piece.OnPieceClickedEvent += OnPieceClicked;
-        AddPiece(piece);
+  private PieceController GetPiecePrefab() {
+    return GetPiecePrefab(UnityEngine.Random.Range(0, 6));
+  }
+
+  private PieceController GetPiecePrefab(int id) {
+    return Resources.Load<PieceController>("Prefabs/" + "Piece" + id);
+  }
+
+  private void OnPieceClicked(PieceController piece) {
+    if(piece != null) {
+      RemovePiece(piece);
+    }
+  }
+
+  private void RemovePiece(PieceController piece) {
+    if(piece != null) {
+      piece.OnPieceClickedEvent -= OnPieceClicked;
+      piece.Remove();
+      if(_pieces != null && _pieces.Contains(piece)) {
+        _pieces.Remove(piece);
       }
+      MovePiecesDown(piece);
+      AddPieceToTop(piece.colIndex);
     }
+  }
 
-    private void AddPiece(PieceController piece) {
-      if(piece != null) {
-        if(_pieces == null) {
-          _pieces = new List<PieceController>();
+  private void MovePiecesDown(PieceController removedPiece) {
+    if(removedPiece != null && _pieces != null) {
+      foreach(PieceController piece in _pieces) {
+        if(piece.colIndex == removedPiece.colIndex && piece.rowIndex > removedPiece.rowIndex) {
+          piece.UpdateRow(piece.rowIndex - 1, GetPiecePositionOnGrid(piece.colIndex, piece.rowIndex - 1));
         }
-        if(!_pieces.Contains(piece)) {
-          _pieces.Add(piece);
-        }
       }
     }
+  }
 
-    private PieceController GetPiecePrefab() {
-      return GetPiecePrefab(UnityEngine.Random.Range(0, 6));
+  private void AddPieceToTop(int colIndex) {
+    int initialRowIndex = rows;
+    var newPiece = InitPiece(colIndex, initialRowIndex);
+    if(newPiece != null) {
+      newPiece.UpdateRow(newPiece.rowIndex - 1, GetPiecePositionOnGrid(colIndex, newPiece.rowIndex - 1));
     }
-
-    private PieceController GetPiecePrefab(int id) {
-      return Resources.Load<PieceController>("Prefabs/" + "Piece" + id);
-    }
-
-    private void OnPieceClicked(PieceController piece) {
-      if(piece != null) {
-        RemovePiece(piece);
-      }
-    }
-
-    private void RemovePiece(PieceController piece) {
-      if(piece != null) {
-        piece.OnPieceClickedEvent -= OnPieceClicked;
-        piece.Remove();
-      }
-    }
+  }
 }
--- Assets/Scripts/PieceController.cs
+++ Assets/Scripts/PieceController.cs
@@ -2,24 +2,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PieceController : MonoBehaviour {
-    public int i;
-    public int j;
-    public event Action<PieceController> OnPieceClickedEvent;
+  public int colIndex;
+  public int rowIndex;
 
-    public void Initialize(int i, int j) {
-      this.i = i;
-      this.j = j;
-    }
+  public float tweenRemoveDuration = 0.5f;
+  public float tweenMoveDonwDuration = 0.5f;
+  public event Action<PieceController> OnPieceClickedEvent;
 
-    public void OnMouseDown() {
-      if(OnPieceClickedEvent != null) {
-        OnPieceClickedEvent(this);
-      }
-    }
+  private SpriteRenderer spriteRenderer;
 
-    public void Remove() {
-      Destroy(gameObject);
+  void Start() {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+
+  public void Initialize(int colIndex, int rowIndex) {
+    this.colIndex = colIndex;
+    this.rowIndex = rowIndex;
+  }
+  
+  public void OnMouseDown() {
+    if(OnPieceClickedEvent != null) {
+      OnPieceClickedEvent(this);
     }
+  }
+
+  public void UpdateRow(int newRowIndex, Vector2 targetPosition) {
+    rowIndex = newRowIndex;
+    transform.DOMove(targetPosition, tweenMoveDonwDuration);
+  }
+
+  public void Remove() {
+    spriteRenderer.sortingOrder++;
+    AnimateRemove();
+    Destroy(gameObject, tweenRemoveDuration);
+  }
+
+  public void AnimateRemove() {
+    transform.DOScale(3f, tweenRemoveDuration);
+    spriteRenderer.DOFade(0f, tweenRemoveDuration);
+  }
 }
